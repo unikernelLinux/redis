0582ba165e5aa6c09ca8d60a6e8dbe83b60dc9f0
vance-raiti <vraiti77@gmail.com>
2024-05-19 13:22:36 +0000

    set bypass and dynamic ukl linkage

--- src/ae.c
+++ src/ae.c
@@ -481,7 +481,13 @@ int aeWait(int fd, int mask, long long milliseconds) {
     }
 }
 
+extern void set_bypass_limit(int val);
+extern void set_bypass_syscall(int val);
+
 void aeMain(aeEventLoop *eventLoop) {
+    set_bypass_limit(10);
+    set_bypass_syscall(1);
+
     eventLoop->stop = 0;
     while (!eventLoop->stop) {
         aeProcessEvents(eventLoop, AE_ALL_EVENTS|
